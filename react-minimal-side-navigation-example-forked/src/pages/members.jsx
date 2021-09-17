fbd661404a4e7332d7ae87ee6a260733c7578e21
shuvam12345 <90901889+shuvam12345@users.noreply.github.com>
2021-09-17 15:42:38 +0530

    Add files via upload

---
+++ react-minimal-side-navigation-example-forked/src/pages/members.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { DashboardLayout } from "../components/Layout";
+
+const MembersPage = () => {
+  return (
+    <DashboardLayout>
+      <h2>Electric Page</h2>
+    </DashboardLayout>
+  );
+};
+
+export default MembersPage;
